6cdd397227092db38266e9dd588d010df7953c75
Pablo Albaladejo <pablo@hireful.co.uk>
2023-12-04 14:21:15 +0100

    fix(04): add SimpleStrategy condition for cards with no wins

--- src/04/scratchcards.test.ts
+++ src/04/scratchcards.test.ts
@@ -4,16 +4,21 @@ import { Scratchcards } from './scratchcards';
 import { SimpleStrategy } from './strategy/simple/simple-strategy';
 import { Card } from './card';
 
-const exampleGames: Card[] = loadCards(
+const example: Card[] = loadCards(
   path.join(__dirname, './fixtures/example.txt')
 );
+const input: Card[] = loadCards(path.join(__dirname, './fixtures/input.txt'));
 
 describe('Solutions', () => {
   describe('Simple strategy', () => {
-    const scratchcards = new Scratchcards(exampleGames,new SimpleStrategy());
-
     test('Example solution', () => {
+      const scratchcards = new Scratchcards(example, new SimpleStrategy());
       expect(scratchcards.solution()).toBe(13);
-    })
-  })
-})
+    });
+
+    test('Input solution', () => {
+      const scratchcards = new Scratchcards(input, new SimpleStrategy());
+      expect(scratchcards.solution()).toBe(18_519);
+    });
+  });
+});
--- src/04/strategy/simple/simple-strategy.ts
+++ src/04/strategy/simple/simple-strategy.ts
@@ -3,6 +3,10 @@ import { Strategy } from '../strategy.interface';
 
 export class SimpleStrategy implements Strategy {
   solution(cards: Card[]): number {
-    return cards.reduce((acc, card) => acc + (2 ^ card.checkWinning()), 0);
+    return cards.reduce(
+      (acc, card) =>
+        card.checkWinning() > 0 ? 2 ** (card.checkWinning() - 1) + acc : acc,
+      0
+    );
   }
 }
