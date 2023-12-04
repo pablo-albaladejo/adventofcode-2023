e4e04f4fcfbee202ce1788ad7471d5beff84dc34
Pablo Albaladejo <pablo@hireful.co.uk>
2023-12-04 15:12:29 +0100

    add(04): add copies methods to card

--- src/04/card/index.test.ts
+++ src/04/card/index.test.ts
@@ -1,15 +1,27 @@
 import { Card } from '.';
 import { loadCards } from '../common/file';
 
-const example: Card[] = loadCards('src/04/fixtures/example.txt');
-
 describe('Card', () => {
   test('checkWinning', () => {
+    const example: Card[] = loadCards('src/04/fixtures/example.txt');
+
     expect(example[0].checkWinning()).toBe(4);
     expect(example[1].checkWinning()).toBe(2);
     expect(example[2].checkWinning()).toBe(2);
     expect(example[3].checkWinning()).toBe(1);
     expect(example[4].checkWinning()).toBe(0);
     expect(example[5].checkWinning()).toBe(0);
+  });
+
+  describe('copies', () => {
+    const card = new Card([], []);
+    test('getCopies', () => {
+      expect(card.getCopies()).toEqual(0);
+    })
+
+    test('addCopy', () => {
+      expect(card.addCopy()).toEqual(1);
+    })
+
   });
 });
--- src/04/card/index.ts
+++ src/04/card/index.ts
@@ -1,6 +1,7 @@
 export class Card {
   winning: number[];
   numbers: number[];
+  copies: number = 0;
 
   constructor(winning: number[], numbers: number[]) {
     this.winning = winning;
@@ -12,5 +13,14 @@ export class Card {
       (acc, number) => (this.winning.includes(number) ? acc + 1 : acc),
       0
     );
+  }
+
+  getCopies(): number {
+    return this.copies;
+  }
+
+  addCopy(): number {
+    this.copies++
+    return this.copies;
   }
 }
