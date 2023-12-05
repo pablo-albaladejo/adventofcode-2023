cefe50c93cb46a28348aea71ce958ae5b9ab25be
Pablo Albaladejo <pablo@hireful.co.uk>
2023-12-05 12:07:54 +0100

    chore(all): parametrize file reader

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,9 +1,16 @@
 import * as fs from 'fs';
 
 export const readLinesFromTextFile = (filePath: string): string[] => {
+  return readByDelimeterFromTextFile(filePath, '\n');
+};
+
+export const readByDelimeterFromTextFile = (
+  filePath: string,
+  delimeter: string
+): string[] => {
   try {
     const data = fs.readFileSync(filePath, 'utf-8');
-    return data.split('\n').map((line) => line.trim());
+    return data.split(delimeter).map((line) => line.trim());
   } catch (error) {
     console.error('Error reading the file:', error);
     return [];
