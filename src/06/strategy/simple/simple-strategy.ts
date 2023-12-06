cd36421735d3a842a75d952325099b15af1b48a4
Pablo Albaladejo <pablo@hireful.co.uk>
2023-12-06 11:47:44 +0100

    feat(06): add simple strategy

--- src/06/strategy/simple/simple-strategy.ts
+++ src/06/strategy/simple/simple-strategy.ts
@@ -1,8 +1,17 @@
-import { Race } from '../../race';
+import { Race, RaceStrategy } from '../../race';
 import { Strategy } from '../strategy.interface';
+
+export const getWinnerStrategies = (race: Race): RaceStrategy[] => {
+  return race
+    .getStrategies()
+    .filter((strategy) => strategy.getDistance() > race.distance);
+};
 
 export class SimpleStrategy implements Strategy {
   solve(races: Race[]): number {
-    throw new Error('Method not implemented.');
+    return races.reduce(
+      (acc, race) => getWinnerStrategies(race).length * acc,
+      1
+    );
   }
 }
