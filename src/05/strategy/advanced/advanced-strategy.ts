4482512a6f86e52b1af14b8580f9cdfeda57fdd5
Pablo Albaladejo <pablo@hireful.co.uk>
2023-12-06 18:59:09 +0100

    fix(05): update advanced strategy

--- src/05/strategy/advanced/advanced-strategy.ts
+++ src/05/strategy/advanced/advanced-strategy.ts
@@ -1,4 +1,4 @@
-import { AlmanacMap, AlmanacSeedMap } from '../../file';
+import { AlmanacMap } from '../../file';
 import { seedLocation } from '../../seeds/location/seed-location';
 import { Strategy } from '../strategy.interface';
 export class AdvancedStrategy implements Strategy {
@@ -8,9 +8,13 @@ export class AdvancedStrategy implements Strategy {
   solve(almanacMap: AlmanacMap): number {
     let result = Number.POSITIVE_INFINITY;
     for (let i = 0; i < almanacMap.seeds.length; i += 2) {
-      for(let j = almanacMap.seeds[i]; j < almanacMap.seeds[i+1]; j++) {
+      for (
+        let j = almanacMap.seeds[i];
+        j < almanacMap.seeds[i] + almanacMap.seeds[i + 1];
+        j++
+      ) {
         result = Math.min(seedLocation(j, almanacMap.seedMaps), result);
-      } 
+      }
     }
     return result;
   }
