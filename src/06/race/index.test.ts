dd17201817f0e52c4c581c52519fd85202eedb88
Pablo Albaladejo <pablo@hireful.co.uk>
2023-12-06 11:34:02 +0100

    feat(06): add race strategies

--- src/06/race/index.test.ts
+++ src/06/race/index.test.ts
@@ -1,3 +1,45 @@
+import { Race, RaceStrategy } from '.';
+import { loadRaces } from '../file';
+import path from 'path';
+
+const example: Race[] = loadRaces(
+  path.join(__dirname, '../fixtures/example.txt')
+);
+
+const raceStrategy1 = new RaceStrategy(0, 7);
+const raceStrategy2 = new RaceStrategy(1, 6);
+const raceStrategy3 = new RaceStrategy(2, 5);
+const raceStrategy4 = new RaceStrategy(3, 4);
+const raceStrategy5 = new RaceStrategy(4, 3);
+const raceStrategy6 = new RaceStrategy(5, 2);
+const raceStrategy7 = new RaceStrategy(6, 1);
+const raceStrategy8 = new RaceStrategy(7, 0);
+
 describe('Race', () => {
-  test('', () => {});
+
+  test('getDistance', () => {
+    expect(raceStrategy1.getDistance()).toBe(0)
+    expect(raceStrategy2.getDistance()).toBe(6)
+    expect(raceStrategy3.getDistance()).toBe(10)
+    expect(raceStrategy4.getDistance()).toBe(12)
+    expect(raceStrategy5.getDistance()).toBe(12)
+    expect(raceStrategy6.getDistance()).toBe(10)
+    expect(raceStrategy7.getDistance()).toBe(6)
+    expect(raceStrategy8.getDistance()).toBe(0)
+  })
+
+  describe('getStrategies', () => {
+    test('race 1', () => {
+      expect(example[0].getStrategies()).toEqual([
+        raceStrategy1,
+        raceStrategy2,
+        raceStrategy3,
+        raceStrategy4,
+        raceStrategy5,
+        raceStrategy6,
+        raceStrategy7,
+        raceStrategy8,
+      ]);
+    });
+  });
 });
--- src/06/race/index.ts
+++ src/06/race/index.ts
@@ -1,9 +1,34 @@
+export class RaceStrategy {
+  charging: number;
+  racing: number;
+
+  constructor(charging: number, racing: number) {
+    this.charging = charging;
+    this.racing = racing;
+  }
+
+  getDistance(): number {
+    return this.charging * this.racing;
+  }
+}
 export class Race {
   time: number;
   distance: number;
+  strategies: RaceStrategy[] = [];
 
   constructor(time: number, distance: number) {
     this.time = time;
     this.distance = distance;
+    this.setStrategies();
+  }
+
+  private setStrategies() {
+    for(let i = 0; i <= this.time; i++) {
+      this.strategies.push(new RaceStrategy(i,this.time-i));
+    }
+  }
+
+  getStrategies(): RaceStrategy[] {
+    return this.strategies;
   }
 }
