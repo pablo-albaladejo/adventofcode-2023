a2464d7015ef5ed86a64525dd666f22f34ce2590
Pablo Albaladejo <pablo@hireful.co.uk>
2023-12-09 13:28:52 +0100

    feat(09): add extrapolate function

--- src/09/history/index.test.ts
+++ src/09/history/index.test.ts
@@ -1,3 +1,20 @@
+import { ReportHistory } from '.';
+import { loadHistories } from '../file';
+import path from 'path';
+
+const example: ReportHistory[] = loadHistories(
+  path.join(__dirname, '../fixtures/example.txt')
+);
 describe('ReportHistory', () => {
-  test('', () => {});
+  describe('extrapolate', () => {
+    test('0 3 6 9 12 15 => 18', () => {
+      expect(example[0].extrapolate()).toEqual(18);
+    });
+    test('1 3 6 10 15 21 => 28', () => {
+      expect(example[1].extrapolate()).toEqual(28);
+    });
+    test('10 13 16 21 30 45 => 68', () => {
+      expect(example[2].extrapolate()).toEqual(68);
+    });
+  });
 });
--- src/09/history/index.ts
+++ src/09/history/index.ts
@@ -1,8 +1,37 @@
+const getLastElement = (array: any[]) => {
+  return array[array.length - 1];
+};
+
 export class ReportHistory {
   private history: number[];
 
   constructor(history: number[]) {
     this.history = history;
   }
-  
+
+  extrapolate(): number {
+    const extrapolated: number[][] = [];
+    let level = 0;
+    extrapolated[level] = Array.from(this.history);
+
+    do {
+      level++;
+      extrapolated[level] = [];
+      for (let i = 1; i < extrapolated[level - 1].length; i++) {
+        extrapolated[level].push(
+          extrapolated[level - 1][i] - extrapolated[level - 1][i - 1]
+        );
+      }
+    } while (!extrapolated[level].every((item) => item === 0));
+
+    extrapolated[level].push(0);
+    do {
+      level--;
+      const leftElement = getLastElement(extrapolated[level]);
+      const belowElement = getLastElement(extrapolated[level + 1]);
+      extrapolated[level].push(leftElement + belowElement);
+    } while (level > 0);
+
+    return extrapolated[0][extrapolated[0].length - 1];
+  }
 }
